78f3ed3cc5aa7d0fc0188972eb2c5590c51a2d45
James Liu <james.liu.vectorspace@gmail.com>
2024-02-20 21:58:47 -0800

    !

--- src/config.js
+++ src/config.js
@@ -1,4 +1,1 @@
-const env = "dev";
-
-export const API_URL =
-  env === "dev" ? "http://127.0.0.1:5000" : "http://64.23.167.149:5000";
+export const API_URL = "http://64.23.167.149:5000"; // "http://127.0.0.1:5000";
